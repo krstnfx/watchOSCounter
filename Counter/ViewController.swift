1e1052b4bc19cb08569a46940225e54b4e5bd379
Kristina Thai <Kristina_Thai@intuit.com>
2016-09-18 15:52:03 -0700

    Add watch connectivity to iPhone app & display data

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,32 +7,80 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 class ViewController: UIViewController {
   
-  @IBOutlet var tableView: UITableView!
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
+    @IBOutlet var tableView: UITableView!
+    var counterData = [Int]()
+    private let session: WCSession? = WCSession.isSupported() ? WCSession.default() : nil
     
-    self.title = "Counter"
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        configureWCSession()
+    }
     
-    self.tableView.delegate = self
-    self.tableView.dataSource = self
-  }
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        configureWCSession()
+    }
+    
+    private func configureWCSession() {
+        session?.delegate = self;
+        session?.activate()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.title = "Counter"
+
+        self.tableView.dataSource = self
+    }
 }
 
-extension ViewController: UITableViewDelegate {
-  //Implement UITableViewDelegate functions here
+extension ViewController: WCSessionDelegate {
+        
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        //Use this to update the UI instantaneously (otherwise, takes a little while)
+        DispatchQueue.main.async {
+            if let counterValue = message["counterValue"] as? Int {
+                self.counterData.append(counterValue)
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    //Handlers in case the watch and phone watch connectivity session becomes disconnected
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
 }
 
 extension ViewController: UITableViewDataSource {
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0;
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
-  }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return counterData.count;
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "CounterCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        let counterValueString = String(counterData[indexPath.row])
+        cell?.textLabel?.text = counterValueString
+        
+        return cell!
+
+    }
 }
